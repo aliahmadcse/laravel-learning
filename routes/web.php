d987752de9fc9df86edf6ceed3ac6d4ccabd5556
aliahmadcse <aliahmadcse@gmail.com>
2020-01-31 18:00:06 +0500

    Adds database route and migrations

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/about','PagesController@about');
 Route::get('/form','PagesController@form');
 
 Route::get('/services','PagesController@services');
+
+Route::resource('posts','PostsController');
