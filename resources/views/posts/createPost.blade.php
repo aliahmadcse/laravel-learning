773c16f69ad2b5f149ab7b3f1fe328a47e2a1b9e
aliahmadcse <aliahmadcse@gmail.com>
2020-02-01 13:30:06 +0500

    Adds post create form

--- resources/views/posts/createPost.blade.php
+++ resources/views/posts/createPost.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.viewdata')
 
 @section('content')
-    <div class="card">
+    <div class="mt-4">
         <h1>Create New Post</h1>
+        {!! Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title','Post Title')}}
+            {{Form::text('txt_title','',['placeholder'=>'Enter title','class'=>'form-control'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('detail','Post Detail')}}
+            {{Form::textarea('txt_detail','',['placeholder'=>'Enter post detail','class'=>'form-control'])}}
+        </div>
+        {{Form::submit('Create Post',['class'=>'btn btn-primary'])}}
+        {!! Form::close() !!}
     </div>    
 @endsection
